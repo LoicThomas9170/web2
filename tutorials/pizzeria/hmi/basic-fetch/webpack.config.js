060a0584e41b9842b8c02c4d6c26b5f77b1c5019
Loic <loic.thomas@student.vinci.be>
2022-11-07 12:31:25 +0100

    auths

--- tutorials/pizzeria/hmi/basic-fetch/webpack.config.js
+++ tutorials/pizzeria/hmi/basic-fetch/webpack.config.js
@@ -79,4 +79,12 @@ module.exports = {
     }),
     new ESLintPlugin(),
   ],
+
+  proxy: {
+    '/api': {
+      target: 'http://localhost:3000',
+      pathRewrite: { '^/api': '' },
+    },
+  },
+
 };
